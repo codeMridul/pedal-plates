8cb289eae981890c504d4f78deed5f342d24cedc
iammridul503 <iammridul503.it@gmail.com>
2020-06-05 20:14:13 +0530

    initial commit

---
+++ app/src/main/java/com/moriarity_code/pedalplates/activity/splashActivity.kt
@@ -0,0 +1,13 @@
+package com.moriarity_code.pedalplates.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moriarity_code.pedalplates.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
