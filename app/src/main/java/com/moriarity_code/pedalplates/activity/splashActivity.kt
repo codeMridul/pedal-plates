6ee4b202940f482878e5c7b5cc087b77d366f07a
iammridul503 <iammridul503.it@gmail.com>
2020-06-06 01:04:44 +0530

    Welcome Screen added

--- app/src/main/java/com/moriarity_code/pedalplates/activity/splashActivity.kt
+++ app/src/main/java/com/moriarity_code/pedalplates/activity/splashActivity.kt
@@ -1,7 +1,11 @@
 package com.moriarity_code.pedalplates.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import com.moriarity_code.pedalplates.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +13,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            window.statusBarColor = ContextCompat.getColor(this@SplashActivity, R.color.statusBar)
+            window.navigationBarColor = ContextCompat.getColor(this@SplashActivity, R.color.statusBar)
+        }
     }
 }
